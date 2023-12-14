555493b60b1879f985b614d1196d3f70bd8b6228
Maksym-Yurii <maksym-iurii.rudko.pz.2020@lpnu.ua>
2023-12-14 04:10:05 +0200

    Added chat navigation from patient list

--- client/src/APPZComponents/VerticalDoctorNavbar.js
+++ client/src/APPZComponents/VerticalDoctorNavbar.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 import '../styles/styles.css';
+import ApplicationPaths from "../paths";
 
-function VerticalDoctorNavbar() {  
-  return (
-    <ButtonGroup vertical size="lg" className="button-group-wrapper">
-      <Button className="btn-style">Профіль</Button>
-      <Button className="btn-style">Пацієнти</Button>
-      <Button className="btn-style">Записи на прийом</Button>
-    </ButtonGroup>
+function VerticalDoctorNavbar() {
+
+
+
+    return (
+        <ButtonGroup vertical size="lg" className="button-group-wrapper">
+            <Button className="btn-style" href={ApplicationPaths.ProfilePage}>Профіль</Button>
+            <Button className="btn-style" href={ApplicationPaths.PatientsPage}>Пацієнти</Button>
+            <Button className="btn-style">Записи на прийом</Button>
+        </ButtonGroup>
     );
 }
 
