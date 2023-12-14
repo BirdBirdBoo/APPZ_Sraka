323e1b75d71a603368a7962c9a9c067814d42f5c
Pavlo-Ivan Mykhalevych <pmykhalevych@tsukat.com>
2023-12-14 03:14:04 +0200

    Add send message written

--- client/src/APPZComponents/MessageTextbox.js
+++ client/src/APPZComponents/MessageTextbox.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { InputGroup, Form, Button } from 'react-bootstrap';
 import img_add_file from '../icons/chat/add_file.png'
 import img_send from '../icons/chat/send.png'
 
 
-function MessageTextbox()
+function MessageTextbox({handleSent})
 {
+  let [messageText, setMessageText] = useState('');
+
+  const handleInputChange = (event) => {
+    setMessageText(event.target.value);
+  };
+
+  const sendMessage = () => {
+    if (messageText.trim() !== '') { 
+      handleSent(messageText);
+      setMessageText('');
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      sendMessage();
+    }
+  };
+
   return (
     <InputGroup className="mb-3" style={{ 
       borderRadius: '30px', 
@@ -14,33 +34,29 @@ function MessageTextbox()
       marginTop: '20px',
       padding: '5px'
     }}>
-        <Button  style={{ 
-        width: '5%',  
-        border: 'none', 
-        backgroundColor: "transparent", 
-        backgroundImage: `url(${img_add_file})`, 
-        backgroundRepeat: "no-repeat", 
-        backgroundPosition: 'center', 
-        backgroundSize: 'contain'
-      }}/>
-        <Form.Control
-            type="text"
-            placeholder="Type your message..."
-            style={{ 
-              width: '80%', 
-              border: 'none', 
-              boxShadow: 'none' 
-            }}
-        />
-        <Button  style={{ 
-        width: '5%',  
-        border: 'none', 
-        backgroundColor: "transparent", 
-        backgroundImage: `url(${img_send})`, 
-        backgroundRepeat: "no-repeat", 
-        backgroundPosition: 'center', 
-        backgroundSize: 'contain'
-      }}/>
+      <Form.Control
+          type="text"
+          placeholder="Type your message..."
+          style={{ 
+            width: '80%', 
+            border: 'none', 
+            boxShadow: 'none' 
+          }}
+          value={messageText}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+      />
+      <Button  style={{ 
+          width: '5%',  
+          border: 'none', 
+          backgroundColor: "transparent", 
+          backgroundImage: `url(${img_send})`, 
+          backgroundRepeat: "no-repeat", 
+          backgroundPosition: 'center', 
+          backgroundSize: 'contain'
+        }}
+        onClick={sendMessage}
+      />
     </InputGroup>
   );
 };
