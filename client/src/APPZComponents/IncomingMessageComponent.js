3d414b735bf28f19f52a9f81879104714d2371aa
Pavlo-Ivan Mykhalevych <pmykhalevych@tsukat.com>
2023-12-18 00:30:56 +0200

    Add link placeholder for annotations

--- client/src/APPZComponents/IncomingMessageComponent.js
+++ client/src/APPZComponents/IncomingMessageComponent.js
@@ -4,11 +4,12 @@ import { Card } from 'react-bootstrap';
 import '../styles/styles.css';
 import img_user from '../icons/chat/user.png'
 
-function IncomingMessageComponent({ senderName, message }){
+function IncomingMessageComponent({senderName, message, messageType}){
   return (
     <div className="incoming-message">
       <Card style={{ border: 'none', boxShadow: 'none' }}>
         <Card.Body className="card-body-incoming">
+          {messageType && <a className='ingoing-annotation-link'>Annotation</a>}
           <div className="message-header">
             <div className="sender-name">{senderName}</div>
             <img src={img_user} alt="User" className="user-icon" />
