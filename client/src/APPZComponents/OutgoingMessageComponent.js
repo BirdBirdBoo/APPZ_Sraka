2c9b5ae89d5395d29c6394b69f41672a49ec37c2
pavloMykhalevych <90715093+pavloMykhalevych@users.noreply.github.com>
2023-12-18 00:33:18 +0200

    Add link placeholder for annotations

--- client/src/APPZComponents/OutgoingMessageComponent.js
+++ client/src/APPZComponents/OutgoingMessageComponent.js
@@ -4,11 +4,12 @@ import { Card } from 'react-bootstrap';
 import '../styles/styles.css';
 import img_user from '../icons/chat/user.png'
 
-function OutgoingMessageComponent({senderName, message}){
+function OutgoingMessageComponent({senderName, message, messageType}){
   return (
     <div className="outgoing-message">
       <Card style={{ border: 'none', boxShadow: 'none' }}>
         <Card.Body className="card-body-outgoing">
+          {messageType && <a className='outgoing-annotation-link'>Annotation</a>}
           <div className="message-header">
             <div className="sender-name">{senderName}</div>
             <img src={img_user} alt="User" className="user-icon" />
