29d4f803dab0701e9dc9c3a4de1b0405d01a3212
Maksym-Yurii.Rudko <maksym-iurii.rudko.pz.2020@lpnu.ua>
2023-12-18 10:16:34 +0200

    Improved Calendar page

--- client/src/APPZComponents/Calendar.js
+++ client/src/APPZComponents/Calendar.js
@@ -1,87 +1,91 @@
 // Calendar.js
-import React, {useState, useEffect } from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import { formatDate } from '../dateFormatter';
+import React, {useState, useEffect} from 'react';
+import {Container, Row, Col, Card} from 'react-bootstrap';
+import {formatDate} from '../dateFormatter';
 
-function Calendar(){
-  // Example data
-  const exampleAppointments = [
-    { id: 1, title: 'Косик Назар Іванович', date: '2023-12-13T12:40:00' },
-    { id: 2, title: 'Юрга Володимир Володимирович', date: '2023-12-15T14:30:00' },
-    { id: 3, title: 'Рудько Максим Юрійович', date: '2023-12-16T16:30:00' },
-    { id: 4, title: 'Косик Назар Іванович', date: '2023-12-14T14:30:00' },
-    { id: 5, title: 'Михалевич Павло-Іван Володимирович', date: '2023-12-17T14:30:00' },
-    { id: 6, title: 'Здрок Дмитро Олександрович', date: '2023-12-14T16:30:00' },
-    { id: 7, title: 'Юрга Володимир Володимирович', date: '2023-12-14T10:30:00' },
-    { id: 8, title: 'Рудько Максим Юрійович', date: '2023-12-14T10:30:00' },
-    { id: 9, title: 'Юрга Володимир Володимирович', date: '2023-12-16T14:30:00' },
-    { id: 10, title: 'Здрок Дмитро Олександрович', date: '2023-12-16T10:30:00' },
-    { id: 11, title: 'Косик Назар Іванович', date: '2023-12-16T12:30:00' },
-    { id: 12, title: 'Михалевич Павло-Іван Володимирович', date: '2023-12-17T12:30:00' },
-    { id: 13, title: 'Рудько Максим Юрійович', date: '2023-12-17T16:30:00' },
-    // Add more appointments as needed
-  ];
+function Calendar() {
+    // Example data
+    const exampleAppointments = [
+        {id: 1, title: 'Косик Назар Іванович', date: '2023-12-13T12:40:00'},
+        {id: 2, title: 'Юрга Володимир Володимирович', date: '2023-12-15T14:30:00'},
+        {id: 3, title: 'Рудько Максим Юрійович', date: '2023-12-16T16:30:00'},
+        {id: 4, title: 'Косик Назар Іванович', date: '2023-12-14T14:30:00'},
+        {id: 5, title: 'Михалевич Павло-Іван Володимирович', date: '2023-12-17T14:30:00'},
+        {id: 6, title: 'Здрок Дмитро Олександрович', date: '2023-12-14T16:30:00'},
+        {id: 7, title: 'Юрга Володимир Володимирович', date: '2023-12-14T10:30:00'},
+        {id: 8, title: 'Рудько Максим Юрійович', date: '2023-12-14T10:30:00'},
+        {id: 9, title: 'Юрга Володимир Володимирович', date: '2023-12-16T14:30:00'},
+        {id: 10, title: 'Здрок Дмитро Олександрович', date: '2023-12-16T10:30:00'},
+        {id: 11, title: 'Косик Назар Іванович', date: '2023-12-16T12:30:00'},
+        {id: 12, title: 'Михалевич Павло-Іван Володимирович', date: '2023-12-17T12:30:00'},
+        {id: 13, title: 'Рудько Максим Юрійович', date: '2023-12-17T16:30:00'},
+        // Add more appointments as needed
+    ];
 
-  // Merge input appointments with example data
-  const [allAppointments, setAllAppointments] = useState([])
+    // Merge input appointments with example data
+    const [allAppointments, setAllAppointments] = useState([])
 
-  const fetchAppointments = () => {
-    try {
-        const sortedAppointments = exampleAppointments.sort((a, b) => new Date(a.date) - new Date(b.date))
+    const fetchAppointments = () => {
+        try {
+            const sortedAppointments = exampleAppointments.sort((a, b) => new Date(a.date) - new Date(b.date))
 
-        const futureAppointments = sortedAppointments.filter(
-          (appointment) => new Date(appointment.date) > new Date()
-        );
+            const futureAppointments = sortedAppointments.filter(
+                (appointment) => new Date(appointment.date) > new Date("2023-12-14T13:00:00")
+            );
 
-        const groupedAppointments = futureAppointments.reduce((acc, appointment) => {
-            const date = new Date(appointment.date).toLocaleDateString();
-            acc[date] = acc[date] || [];
-            acc[date].push(appointment);
-            return acc;
-          }, {});
+            const groupedAppointments = futureAppointments.reduce((acc, appointment) => {
+                const date = new Date(appointment.date).toLocaleDateString();
+                acc[date] = acc[date] || [];
+                acc[date].push(appointment);
+                return acc;
+            }, {});
 
-        setAllAppointments(groupedAppointments);
-    } catch (error) {
-        console.error("Could not fetch messages:", error);
-    }
-  };
+            setAllAppointments(groupedAppointments);
+        } catch (error) {
+            console.error("Could not fetch messages:", error);
+        }
+    };
 
-  useEffect(() => {
-    fetchAppointments();
-  }, []); 
+    useEffect(() => {
+        fetchAppointments();
+    }, []);
 
-  return (
-    <Card style={{
-      border: 'none',
-      boxShadow: 'none',
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column'
-  }}>
-        <Card.Body style={{ 
-                overflowY: 'auto',
+    return (
+        <Card style={{
+            border: 'none',
+            boxShadow: 'none',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <Card.Body style={{
                 height: '100%',
-                overflowY: 'auto'}}>
-            <Container>
-              {Object.keys(allAppointments).map((date) => (
-                  <Row key={date} className="mt-4">
-                  <Col>
-                      <h3>{formatDate(date)}</h3>
-                      {allAppointments[date].map((appointment) => (
-                      <Card key={appointment.id} className="mb-2">
-                          <Card.Body>
-                          <Card.Title>{appointment.title}</Card.Title>
-                          <Card.Text>{new Date(appointment.date).toLocaleTimeString()}</Card.Text>
-                          </Card.Body>
-                      </Card>
-                      ))}
-                  </Col>
-                  </Row>
-              ))}
-            </Container>
-        </Card.Body>
-    </Card>
-  );
+                overflowY: 'auto'
+            }}>
+                <Container>
+                    <h1>Мої записи</h1>
+                    <Col>
+                        <button className="btn-style w-auto ms-1 mb-3 mt-2 px-4">Додати запис</button>
+                    </Col>
+                    {Object.keys(allAppointments).map((date) => (
+                        <Row key={date} className="mt-4">
+                            <Col>
+                                <h3>{formatDate(date)}</h3>
+                                {allAppointments[date].map((appointment) => (
+                                    <Card key={appointment.id} className="mb-2">
+                                        <Card.Body>
+                                            <Card.Title>{appointment.title}</Card.Title>
+                                            <Card.Text>{new Date(appointment.date).toLocaleTimeString()}</Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                ))}
+                            </Col>
+                        </Row>
+                    ))}
+                </Container>
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default Calendar;
