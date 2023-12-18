cd16b1b8f4021f90a28fd3cf70c509fce268a725
funwareDev <kosyk.nazar@gmail.com>
2023-12-18 04:17:32 +0200

    now links in anotation messages forward to analyzes

--- client/src/APPZComponents/IncomingMessageComponent.js
+++ client/src/APPZComponents/IncomingMessageComponent.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Card } from 'react-bootstrap';
+import ApplicationPaths from '../paths';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 import '../styles/styles.css';
 import img_user from '../icons/chat/user.png'
 import img_link from '../icons/chat/link.png'
 
-function IncomingMessageComponent({senderName, message, messageType}){
+function IncomingMessageComponent({ messageId, senderName, message, messageType }) {
+  const [analysisId, setAnalysisId] = useState('')
+
+  function getAnalysisIdByMessageId(id) {
+    let result = ''
+    axios.get('https://localhost:7130/api/Annotations/getAnalysisId', {
+      params: {
+        "messageId": id
+      }
+    }).then(res => {
+      console.log("anal id: " + res.data)
+      result = res.data
+      setAnalysisId(result)
+    })
+  }
+
+  if (messageType == 1) {
+    getAnalysisIdByMessageId(messageId)
+  }
+
   return (
     <div className="incoming-message">
       <Card style={{ border: 'none', boxShadow: 'none' }}>
         <Card.Body className="card-body-incoming">
-          {messageType==1 && <a className='ingoing-annotation-link'>Annotation</a>}
-          {messageType==1 && <a><img src={img_link} alt="Link" className="link-icon-incoming" /></a>}
+          {messageType == 1 && <a className='ingoing-annotation-link'>Annotation</a>}
+          {messageType == 1 && <NavLink to={`${ApplicationPaths.AnalysisPage}?analysis_id=${analysisId}`}>
+            <img src={img_link} alt="Link" className="link-icon-incoming" />
+          </NavLink>
+          }
           <div className="message-header">
             <div className="sender-name">{senderName}</div>
             <img src={img_user} alt="User" className="user-icon" />
--- client/src/APPZComponents/OutgoingMessageComponent.js
+++ client/src/APPZComponents/OutgoingMessageComponent.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Card } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import ApplicationPaths from '../paths';
+import axios from 'axios';
 
 import '../styles/styles.css';
 import img_user from '../icons/chat/user.png'
 import img_link from '../icons/chat/link_out.png'
 
-function OutgoingMessageComponent({senderName, message, messageType}){
+const navLinkStyle = { textDecoration: "none" };
+
+function OutgoingMessageComponent({ messageId, senderName, message, messageType }) {
+  const [analysisId, setAnalysisId] = useState('')
+
+   function getAnalysisIdByMessageId(id) {
+    let result = ''
+    axios.get('https://localhost:7130/api/Annotations/getAnalysisId', {
+      params: {
+        "messageId": id
+      }
+    }).then(res =>
+      {
+        console.log("anal id: " + res.data)
+        result = res.data
+        setAnalysisId(result)
+      })
+  }
+
+  if(messageType==1){
+    getAnalysisIdByMessageId(messageId)
+  }
+
   return (
     <div className="outgoing-message">
       <Card style={{ border: 'none', boxShadow: 'none' }}>
         <Card.Body className="card-body-outgoing">
-          {messageType==1 && <a className='outgoing-annotation-link'>Annotation</a>}
-          {messageType==1 && <a><img src={img_link} alt="User" className="link-icon-outcoming" /></a>}
+          {messageType == 1 && <a className='outgoing-annotation-link'>Annotation</a>}
+          {messageType == 1 && 
+          <NavLink to={`${ApplicationPaths.AnalysisPage}?analysis_id=${analysisId}`}>
+            <img src={img_link} alt="User" className="link-icon-outcoming" />
+            </NavLink>}
           <div className="message-header">
             <div className="sender-name">{senderName}</div>
             <img src={img_user} alt="User" className="user-icon" />
